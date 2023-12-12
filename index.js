8d72f538df73dc40d10e1d23a0737439fcc2d5a0
kshitij varshney <kshitijvarshney92@gmail.com>
2023-12-13 02:54:59 +0530

    commit

--- index.js
+++ index.js
@@ -66,8 +66,8 @@ app.use((error, req, res, next) => {
 });
 
 // server
-const PORT = 80;
-// app.listen(PORT, () => {
-//   console.log(`Server running on port ${PORT}`);
-// });
-app.listen(PORT, "0.0.0.0");
+const PORT = 3000;
+app.listen(PORT, () => {
+  console.log(`Server running on port ${PORT}`);
+});
+// app.listen(PORT, "0.0.0.0");
